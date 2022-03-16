f1610ab3bd4d77157ecd9f7e4ec6cdc3c9d98caf
alamiGanLS <lsubandi@alamisharia.co.id>
2022-03-16 14:29:28 +0700

    first commit

---
+++ src/app/tab2/rating/rating.component.ts
@@ -0,0 +1,68 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+@Component({
+	selector: "app-rating",
+	templateUrl: "./rating.component.html",
+	styleUrls: ["./rating.component.scss"],
+})
+export class RatingComponent implements OnInit {
+	@Input() rating: number;
+
+	@Output() ratingChange: EventEmitter<number> = new EventEmitter();
+
+	constructor() {}
+
+	ngOnInit() {}
+
+	rate(index: number) {
+		// function used to change the value of our rating
+		// triggered when user, clicks a star to change the rating
+		this.rating = index;
+		this.ratingChange.emit(this.rating);
+	}
+
+	isAboveRating(index: number): boolean {
+		// returns whether or not the selected index is above ,the current rating
+		// function is called from the getColor function.
+		return index > this.rating;
+	}
+
+	getColor(index: number) {
+		/* function to return the color of a star based on what
+       index it is. All stars greater than the index are assigned
+       a grey color , while those equal or less than the rating are
+       assigned a color depending on the rating. Using the following criteria:
+
+            1-2 stars: red
+            3 stars  : yellow
+            4-5 stars: green
+      */
+
+		if (this.isAboveRating(index)) {
+			return COlLORS.GREY;
+		}
+		switch (this.rating) {
+			case 1:
+				return COlLORS.RED;
+			case 2:
+				return COlLORS.ORANGE;
+			case 3:
+				return COlLORS.YELLOW;
+			case 4:
+				return COlLORS.GREEN;
+			case 5:
+				return COlLORS.BLUE;
+			default:
+				return COlLORS.GREY;
+		}
+	}
+}
+
+enum COlLORS {
+	GREY = "#E0E0E0",
+	GREEN = "#76FF03",
+	YELLOW = "#FFCA28",
+	RED = "#DD2C00",
+	BLUE = "#0060ff",
+	ORANGE = " #ff8000",
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PopoverController } from "@ionic/angular";
+import { PopoverComponent } from "./popover/popover.component";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: "app-tab2",
+	templateUrl: "tab2.page.html",
+	styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+	constructor(public popoverController: PopoverController) {}
+	async presentPopover(ev: any) {
+		const popover = await this.popoverController.create({
+			component: PopoverComponent,
+			cssClass: "my-custom-class",
+			event: ev,
+			translucent: true,
+		});
+		await popover.present();
 
-  constructor() {}
-
+		const { role } = await popover.onDidDismiss();
+		console.log("onDidDismiss resolved with role", role);
+	}
 }
